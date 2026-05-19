074952acbe85a93c7c7557516ac29f4434818195
SigmaArchie <LehnerA22086@sus.htl-grieskirchen.at>
2026-05-19 08:32:53 +0200

    Setup Express backend with Socket.IO

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:5173'
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+app.get('/', (req, res) => {
+  res.send('Backend is running');
+});
+
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
